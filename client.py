de355715cf9d6b9be4209e2f183435958e1e1ecd
ravi4all <ravikanttyagi.2295@gmail.com>
2019-07-05 17:54:38 +0530

    Add files via upload

---
+++ client.py
@@ -0,0 +1,31 @@
+import socket
+import sys
+
+def main():
+    soc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    host = "192.168.1.6"
+    port = 80
+
+    try:
+        soc.connect((host, port))
+    except:
+        print("Connection error")
+        sys.exit()
+
+    print("Enter 'quit' to exit")
+    username = input("Enter your name : ")
+    soc.sendall(username.encode("utf8"))
+
+    message = input(" -> ")
+
+    while message != 'quit':
+        soc.sendall(message.encode("utf8"))
+        data = soc.recv(1024)
+        print(data.decode())
+
+        message = input(" -> ")
+
+    soc.send(b'--quit--')
+
+if __name__ == "__main__":
+    main()
